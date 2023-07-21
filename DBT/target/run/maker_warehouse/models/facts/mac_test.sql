56b68158dde52c7bf93aacd7184e609857ee432b
Ilhaam <ilhaam777@gmail.com>
2023-07-21 10:23:29 +0100

    "changes commited"

---
+++ DBT/target/run/maker_warehouse/models/facts/mac_test.sql
@@ -0,0 +1,17 @@
+
+  create view "mydb"."dbt_warehouse"."mac_test__dbt_tmp"
+    
+    
+  as (
+    
+
+select
+
+    
+    'bank_transfer'
+    
+    'credit_card'
+    
+    'gift_card'
+    
+  );
